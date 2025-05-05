a1178854fb8db469abb8b200f33282c9d30c7def
Eduardo.fbarreto <fbarreto.eduardo@gmail.com>
2025-05-04 22:05:42 -0300

    criando conexões

--- conn.php
+++ conn.php
@@ -1,8 +1,11 @@
 <?php
 
   $nome = $_POST["nome"];
+  $sobrenome = $_POST["sobrenome"];
   $idade = $_POST["idade"];
+  $email = $_POST["email"];
 
-  echo "O nome digitado foi: " . htmlspecialchars($nome);
+  echo "O nome digitado foi: " . htmlspecialchars($nome) . " " . htmlspecialchars($sobrenome) .".";
   echo "<br> A idade é de: " . htmlspecialchars($idade);
+  echo "<br>Seu e-mail é: " . htmlspecialchars($email);
 ?>
